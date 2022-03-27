b6388e5c7f695f7eb3ce7ea88fe8177b43ac8d09
Jeff Kaseman <jjkaseman@gmail.com>
2022-03-27 19:24:50 -0400

    Clean up mix n match async code

--- src/ramdaPulls.js
+++ src/ramdaPulls.js
@@ -8,28 +8,32 @@ export async function getAllPRs() {
         let morePRs = true;
         let prList = [];
         
-        while (morePRs){
-            // 'state = aLL' pulls open & closed
-            await fetch('https://api.github.com/repos/ramda/ramda/pulls?state=all&per_page=' + maxPRs + '&page=' + pageCount + '&direction=asc', { 
-                method: 'GET',
-                headers: {
-                    'Authorization': 'token ' + pat,    // This allows 5000 API calls per hour vs. 60 per hour
-                }})
-                .then(response => response.json())
-                .then(data => { 
-                    if (data.length > 0){
-                        prList = prList.concat(data);
-                    }
-                    // console.log("Page " + pageCount);
-                    
-                    if (data.length < maxPRs){
-                        morePRs = false;
-                    }
-                    else{
-                        pageCount++;
-                    }
-                })
-                .catch(error => console.error(error));
+        try {
+            while (morePRs){
+                // 'state = aLL' pulls open & closed
+                let response = await fetch('https://api.github.com/repos/ramda/ramda/pulls?state=all&per_page=' + maxPRs + '&page=' + pageCount + '&direction=asc', { 
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'token ' + pat,    // This allows 5000 API calls per hour vs. 60 per hour
+                    }});
+                let data = await response.json();
+                
+                if (data.length > 0){
+                    prList = prList.concat(data);
+                }
+                // console.log("Page " + pageCount);
+                        
+                if (data.length < maxPRs){
+                    morePRs = false;
+                }
+                else{
+                    pageCount++;
+                }
+            }
+        }
+        catch (error){
+            console.log('Error thrown while retrieving page #' + pageCount);
+            console.error(error);
         }
         return prList;
     }
