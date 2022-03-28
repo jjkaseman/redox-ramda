cf61dce6539027f9c9709e116a47760a91cf24f7
Jeff Kaseman <jjkaseman@gmail.com>
2022-03-28 11:32:08 -0400

    Sort PRs from each repo into the same list

--- src/sortWeeks.js
+++ src/sortWeeks.js
@@ -1,26 +1,30 @@
 export function getWeeklyPRs(prList){
     let weeklyPRs = [];
 
-    for (let i = 0; i < prList.length; i++){
-        let pr = prList[i];
+    // Combine all repos into the same list
+    for (let repo in prList){
 
-        // Not merged, no point in tracking it
-        if (!pr.merged_at || pr.merged_at === null){
-            continue;
-        }
+        for (let i = 0; i < prList[repo].length; i++){
+            let pr = prList[repo][i];
 
-        let prMergeTime = new Date(Date.parse(pr.merged_at));
-        // Find the Sunday before the PR    // 86400000 = 1 day in milliseconds
-        let prMergeWeekDate = new Date((Math.floor(prMergeTime/86400000) - prMergeTime.getUTCDay()) * 86400000);
-        // format as YYYY-MM-DD     // get[UTC]Month starts at 0... 
-        let prMergeWeek = prMergeWeekDate.getUTCFullYear() + '-' + ('00' + (prMergeWeekDate.getUTCMonth() + 1)).slice(-2) + '-' + ('00' + prMergeWeekDate.getUTCDate()).slice(-2);
+            // Not merged, no point in tracking it
+            if (!pr.merged_at || pr.merged_at === null){
+                continue;
+            }
 
-        // Add PRs for each week, or create a new entry for the week
-        if (weeklyPRs[prMergeWeek]){
-            weeklyPRs[prMergeWeek].push(pr);
-        }
-        else{
-            weeklyPRs[prMergeWeek] = [pr];
+            let prMergeTime = new Date(Date.parse(pr.merged_at));
+            // Find the Sunday before the PR    // 86400000 = 1 day in milliseconds
+            let prMergeWeekDate = new Date((Math.floor(prMergeTime/86400000) - prMergeTime.getUTCDay()) * 86400000);
+            // format as YYYY-MM-DD     // get[UTC]Month starts at 0... 
+            let prMergeWeek = prMergeWeekDate.getUTCFullYear() + '-' + ('00' + (prMergeWeekDate.getUTCMonth() + 1)).slice(-2) + '-' + ('00' + prMergeWeekDate.getUTCDate()).slice(-2);
+
+            // Add PRs for each week, or create a new entry for the week
+            if (weeklyPRs[prMergeWeek]){
+                weeklyPRs[prMergeWeek].push(pr);
+            }
+            else{
+                weeklyPRs[prMergeWeek] = [pr];
+            }
         }
     }
     
