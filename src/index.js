c043f97d8d588a7f5c6b49b7d4bd3ce4291e3ece
Jeff Kaseman <jjkaseman@gmail.com>
2022-03-28 11:33:47 -0400

    Get PRs for all repos and display the details.  Move handling code into their own functions to unclutter the main program.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
+import { getAllRepos } from './ramdaRepos.js';
 import { getAllPRs } from './ramdaPulls.js';
-import {getWeeklyPRs} from './sortWeeks.js';
+import { getWeeklyPRs } from './sortWeeks.js';
 
 console.clear();
-console.log('This may take a minute.  Please wait...');     // ~40 seconds
+console.log('This may take a minute.  Please wait...');     // ~45 seconds for all repos
 
-let prList = await getAllPRs();
-
-if (!prList || prList.length === 0)
+let prList = await gatherAllPRs();
+if (!prList)
 {
     console.error('**No PRs found.**');
     process.exit();
 }
 
-let prWeeks = getWeeklyPRs(prList);
-let totalMerges = 0;
-for (var prWeek in prWeeks){
-    totalMerges += prWeeks[prWeek].length;
-    console.log(prWeek + " - PRs this week: " + prWeeks[prWeek].length);
-    for (let i = 0; i < prWeeks[prWeek].length; i++){
-        console.log('\tPR#' + prWeeks[prWeek][i].number);
-    }
+let totalPRs = countPRs(prList);
+if (!totalPRs || totalPRs === 0){
+    console.error('**Zero PRs found.**');
+    process.exit();
 }
 
-console.log('\nTotal PRs found: ' + prList.length);
-console.log('Total merges: ' + totalMerges);
+let totalMerges = displayPRs(prList);
+console.log('\nTotal PRs found: ' + totalPRs);
+console.log('Total PRs merged: ' + totalMerges);
+
+// Not needed, but it's a good visual separation from the main program
+process.exit();
+
+async function gatherAllPRs(){    
+    let repoList = await getAllRepos();
+    let prList = [];
+
+    for (let i = 0; i < repoList.length; i++){
+        let repoName = repoList[i].name;
+        prList[repoName] = await getAllPRs(repoName);
+    }
+    return prList;
+}
+
+function countPRs(prList){
+    let totalPRs = 0;
+
+    for (let repo in prList){
+        totalPRs += Object.keys(prList[repo]).length;
+    }
+
+    return totalPRs;
+}
+
+function displayPRs(prList){
+    let prWeeks = getWeeklyPRs(prList);
+    let totalMerges = 0;
+    for (var prWeek in prWeeks){
+        totalMerges += prWeeks[prWeek].length;
+        console.log(prWeek + " - PRs this week: " + prWeeks[prWeek].length);
+        for (let i = 0; i < prWeeks[prWeek].length; i++){
+            console.log('\t' + prWeeks[prWeek][i].base.repo.name + ' - PR#' + prWeeks[prWeek][i].number);
+        }
+    }
+
+    return totalMerges;
+}
