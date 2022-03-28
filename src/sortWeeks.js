30bffe2a0c14e56b2e141e0658786afbb84ed5e0
Jeff Kaseman <jjkaseman@gmail.com>
2022-03-27 21:13:18 -0400

    Return PRs sorted by merge date

--- src/sortWeeks.js
+++ src/sortWeeks.js
@@ -12,8 +12,8 @@ export function getWeeklyPRs(prList){
         let prMergeTime = new Date(Date.parse(pr.merged_at));
         // Find the Sunday before the PR    // 86400000 = 1 day in milliseconds
         let prMergeWeekDate = new Date((Math.floor(prMergeTime/86400000) - prMergeTime.getUTCDay()) * 86400000);
-        // get[UTC]Month starts at 0... 
-        let prMergeWeek = prMergeWeekDate.getUTCFullYear() + '-' + (prMergeWeekDate.getUTCMonth() + 1) + '-' + prMergeWeekDate.getUTCDate();
+        // format as YYYY-MM-DD     // get[UTC]Month starts at 0... 
+        let prMergeWeek = prMergeWeekDate.getUTCFullYear() + '-' + ('00' + (prMergeWeekDate.getUTCMonth() + 1)).slice(-2) + '-' + ('00' + prMergeWeekDate.getUTCDate()).slice(-2);
 
         // Add PRs for each week, or create a new entry for the week
         if (weeklyPRs[prMergeWeek]){
@@ -23,6 +23,13 @@ export function getWeeklyPRs(prList){
             weeklyPRs[prMergeWeek] = [pr];
         }
     }
-
-    return weeklyPRs;
+    
+    // Return PRs based on merge date, not create date
+    return Object.keys(weeklyPRs).sort().reduce(
+        (obj, key) => {
+            obj[key] = weeklyPRs[key];
+            return obj;
+        },
+        {}
+    );
 }
